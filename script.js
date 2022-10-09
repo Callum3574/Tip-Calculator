31c6d200aaab5a0ebb6dc1d05ca79be417822787
Callum Hall <callumhall@Callums-MacBook-Pro.local>
2022-10-09 16:40:09 +0100

    Tip Calculator v1

---
+++ script.js
@@ -0,0 +1,42 @@
+const array = [];
+
+function addAnother() {
+  const enterInformation = prompt(`Would you like to add a new input?`);
+  return enterInformation;
+}
+
+function pushToArray(runCalc) {
+  while (addAnother() === "yes") {
+    const newEntry = runCalc();
+    array.push(newEntry);
+    console.log(newEntry);
+  }
+  return array;
+}
+
+function runCalc() {
+  function foodPlace() {
+    const moneySpentAt = prompt(`where did you spend your money?`);
+    return moneySpentAt;
+  }
+  const bill = Number(prompt(`please enter the price of the bill`));
+  function tipCalc(bills) {
+    let tip = 0;
+    if (bills < 50) {
+      tip = bills * 0.3;
+    } else if (bills >= 50 && bills < 100) {
+      tip = bills * 0.25;
+    } else if (bills >= 100) {
+      tip = bills * 0.2;
+    }
+    return tip;
+  }
+  return {
+    Location: foodPlace(),
+    bill: bill,
+    Total_Tip: tipCalc(bill),
+    Overall_Bill: tipCalc(bill) + bill,
+  };
+}
+
+console.log(pushToArray(runCalc));
