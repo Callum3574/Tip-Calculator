f4a4c38eb61483ece76c63caa38dbf107a50a850
Callum Hall <callumhall@Callums-MacBook-Pro.local>
2022-10-09 22:37:21 +0100

    Tip Calc v5

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
 const arrayOfInputs = [];
 
-function addAnother() {
-  const enterInformation = prompt(`Would you like to add a new input?`);
-  return enterInformation;
-}
-
-function pushToArray(array) {
-  while (addAnother() === "yes") {
-    const newEntry = runCalc();
-    array.push(newEntry);
-    console.log(newEntry);
-  }
-  return array;
-}
-
 function runCalc() {
   const moneySpentAt = prompt(`where did you spend your money?`);
   const bill = Number(prompt(`please enter the price of the bill`));
@@ -35,6 +21,38 @@ function runCalc() {
     Total_Tip: tipCalc(bill),
     Overall_Bill: tipCalc(bill) + bill,
   };
+}
+
+function addAnother() {
+  const enterInformation = prompt(`Would you like to add a new input?`);
+  return enterInformation;
+}
+
+function pushToArray(array) {
+  while (addAnother() === "yes") {
+    const newEntry = runCalc();
+    array.push(newEntry);
+    console.log(newEntry);
+  }
+  return sortArray(arrayOfInputs);
+}
+
+function sortArray(array) {
+  const highestOrLowest = prompt(
+    `Would you like to know the highest bill or lowest?`
+  );
+
+  let sorted = array.sort(function (a, b) {
+    return a.Overall_Bill - b.Overall_Bill;
+  });
+  if (highestOrLowest === `highest`) {
+    sorted = sorted[sorted.length - 1];
+  } else if (highestOrLowest === "lowest") {
+    sorted = sorted[0];
+  } else if (highestOrLowest === "no") {
+    return array;
+  }
+  return sorted;
 }
 
 console.log(pushToArray(arrayOfInputs));
